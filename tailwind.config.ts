d77bc7e8ea7f136dfd624e577c09eec35d1659f5
Leila19t <tarabih.leila@gmail.com>
2025-01-09 16:44:46 +0400

    updated animaation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,7 +51,7 @@ export default {
       animation: {
         flash: "flash 1s step-start infinite",
         rotate: "rotate 10s linear infinite;",
-        flip: "flip 0.6s infinite linear",
+        flip: "flip 0.8s ease-in-out infinite ",
       },
     },
   },
